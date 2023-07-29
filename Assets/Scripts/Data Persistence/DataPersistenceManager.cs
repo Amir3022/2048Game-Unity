fa63fcbe0c792105d7ce42b08c7f83dbb19b485a
Amir Tayie <amir.tayie@corporatica.com>
2023-07-29 12:14:48 +0300

    Created the data persistence manager, file data handler classes, created IDataPersistence interface

--- Assets/Scripts/Data Persistence/DataPersistenceManager.cs
+++ Assets/Scripts/Data Persistence/DataPersistenceManager.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 using UnityEngine;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using System;
 
-public class DataPre : MonoBehaviour
+public class DataPersistenceManager: MonoBehaviour
 {
+    [Header("File Storage Config")]
+    [SerializeField] private bool UseEncryption;
+    [SerializeField] private string Filename;
+
+    private string SaveGamePath;
+
+    private List<IDataPersistence> dataPersistances;
+    private GameData gameData;
+    private FileDataHandler dataHandler;
+
+    public static DataPersistenceManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if(!instance)
+        {
+            Debug.Log("Only one instance of the Data Persistence manager can be created per game");
+        }
+        instance = this;
+        SaveGamePath = Path.Combine(Application.dataPath, "SaveGames");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dataPersistances = GetDataPersistenceObjects();
+        dataHandler = new FileDataHandler(SaveGamePath, Filename, UseEncryption);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewGame()
     {
-        
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        if(dataHandler != null)
+        {
+            gameData = dataHandler.Load();
+            if(gameData != null) 
+            {
+                foreach(IDataPersistence dataPersistence in dataPersistances) 
+                {
+                    dataPersistence.LoadData(gameData);
+                }
+            }
+            else
+            {
+                Debug.Log("Loaded data was empty, creating new game data");
+                NewGame();
+            }
+        }
+    }
+
+    public void SaveGame()
+    {
+        if(dataHandler!=null) 
+        {
+            foreach(IDataPersistence dataPersistence in dataPersistances)
+            {
+                dataPersistence.SaveData(ref gameData);
+            }
+            dataHandler.Save(gameData);
+        }
+    }
+
+    private List<IDataPersistence> GetDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistencebjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+        return new List<IDataPersistence>(dataPersistencebjects);  
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame(); 
     }
 }
---
+++ Assets/Scripts/Data Persistence/FileDataHandler.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class FileDataHandler
+{
+    private bool bUseEncryption;
+    private string fileName;
+    private string folderPath;
+    private readonly string EncryptionWord = "Cipher";
+    public FileDataHandler(string folderPath, string FileName, bool bUseEncrytion)
+    {
+        this.folderPath = folderPath;
+        this.fileName= FileName;
+        this.bUseEncryption= bUseEncrytion;
+    }
+
+    public GameData Load()
+    {
+        string FullPath = Path.Combine(folderPath, this.fileName);
+        GameData LoadedData = null;
+        if(File.Exists(FullPath))
+        {
+            try 
+            {
+                string dataToRead = "";
+                using(FileStream stream = new FileStream(FullPath, FileMode.Open))
+                {
+                    using(StreamReader reader = new StreamReader(stream)) 
+                    {
+                        dataToRead = reader.ReadToEnd();
+                    }
+                }
+                if(bUseEncryption) 
+                {
+                    dataToRead = EncryptDecrypt(dataToRead);
+                }
+                LoadedData = JsonUtility.FromJson<GameData>(dataToRead);
+            }
+            catch(Exception e)
+            {
+                Debug.Log($"Unable to load Saved game from path: {FullPath} \n{e}");
+            }
+        }
+        return LoadedData;
+    }
+
+    public void Save(GameData gameData)
+    {
+        string FullPath = Path.Combine(folderPath, fileName);
+        try
+        {
+            Directory.CreateDirectory(folderPath);
+            string dataToStore = JsonUtility.ToJson(gameData, true);
+            if(bUseEncryption)
+            {
+                dataToStore = EncryptDecrypt(dataToStore);
+            }
+            using(FileStream stream = new FileStream(FullPath, FileMode.Create))
+            {
+                using(StreamWriter writer = new StreamWriter(stream)) 
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.Log($"Unable to load to file with path: {FullPath}. \n {e}");
+        }
+    }
+
+    private string EncryptDecrypt(string data)
+    {
+        string EncryptedWord = "";
+        for(int i = 0; i < data.Length; i++) 
+        {
+            EncryptedWord += data[i] ^ EncryptionWord[i% EncryptionWord.Length];
+        }
+        return EncryptedWord;
+    }
+}
